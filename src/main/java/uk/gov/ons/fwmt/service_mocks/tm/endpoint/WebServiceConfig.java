aa5c5394ee5a9dee0982f9cccb4e5cb8df06007f
ThomasPootQA <thomas.poot@qa.com>
2018-08-15 12:35:41 +0100

    Fixed the package name of the config class

--- src/main/java/uk/gov/ons/fwmt/service_mocks/tm/endpoint/WebServiceConfig.java
+++ src/main/java/uk/gov/ons/fwmt/service_mocks/tm/endpoint/WebServiceConfig.java
@@ -1,4 +1,4 @@
-package uk.gov.ons.fwmt.service_mocks.tm.ws;
+package uk.gov.ons.fwmt.service_mocks.tm.endpoint;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.web.servlet.ServletRegistrationBean;
